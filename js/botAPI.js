db2adf341cfde21b41fbae6f761d718370e8955f
frontMan <mr.cdrmn277@gmail.com>
2024-11-20 18:22:11 +0300

    next try to fix theme colors

--- js/botAPI.js
+++ js/botAPI.js
@@ -9,16 +9,26 @@ function applyTelegramTheme() {
     document.body.style.backgroundColor = theme.bg_color;
     document.body.style.color = theme.text_color;
 
+
+    if (theme.bg_color === "#000000") {
+        document.body.style.color = "#ffffff";
+        document.querySelectorAll("*").forEach(element => {
+            element.style.color = "#ffffff";
+        });
+    } else {
+        document.body.style.color = "#000000";
+        document.querySelectorAll("*").forEach(element => {
+            element.style.color = "#000000";
+        });
+    }
     modalElements.forEach((modal) => {
         modal.style.backgroundColor = theme.bg_color;
         modal.style.color = theme.text_color;
     });
-    document.querySelectorAll("*").forEach(element => {
-        element.style.color = theme.text_color;
-        element.style.backgroundColor = theme.bg_color;
-    });
+
     document.querySelectorAll("button").forEach(element => {
         element.style.backgroundColor = "#E31E24";
+        element.style.color = "#FFFFFF";
     });
 }
 applyTelegramTheme();
