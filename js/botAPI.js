9c9e2703e3f641c753f394575a69a26e654e4b48
frontMan <mr.cdrmn277@gmail.com>
2024-11-20 18:06:49 +0300

    try to fix theme colors of text and containers

--- js/botAPI.js
+++ js/botAPI.js
@@ -1,13 +1,25 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.expand();
 
 function applyTelegramTheme() {
     const theme = Telegram.WebApp.themeParams;
-    let modal = document.querySelectorAll(".modalWrapper")
+    const modalElements = document.querySelectorAll(".modalWrapper");
+
     document.body.style.backgroundColor = theme.bg_color;
-    modal.style.backgroundColor = theme.bg_color;
     document.body.style.color = theme.text_color;
+
+    modalElements.forEach((modal) => {
+        modal.style.backgroundColor = theme.bg_color;
+        modal.style.color = theme.text_color;
+    });
+    document.querySelectorAll("*").forEach(element => {
+        element.style.color = theme.text_color;
+        element.style.backgroundColor = theme.bg_color;
+    });
+    document.querySelectorAll("button").forEach(element => {
+        element.style.backgroundColor = "#E31E24";
+    });
 }
 applyTelegramTheme();
 
